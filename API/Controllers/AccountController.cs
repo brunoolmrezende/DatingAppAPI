911ef98721bf473e94a311080e40ab3a034fd059
brunoolmrezende <brunoolmrezende303@gmail.com>
2025-08-14 15:21:00 -0300

    feat: implement login functionality and create LoginDto for user authentication

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -35,6 +35,32 @@ public class AccountController(AppDbContext dbContext) : DatingAppController
         return Created(string.Empty, user);
     }
 
+    [HttpPost]
+    [Route("login")]
+    public async Task<ActionResult<AppUser>> Login([FromBody] LoginDto request)
+    {
+        var user = await dbContext.Users.SingleOrDefaultAsync(user => user.Email.ToLower() == request.Email.ToLower());
+
+        if (user is null)
+        {
+            return Unauthorized("Invalid email or password");
+        }
+
+        using var hmac = new HMACSHA512(user.PasswordSalt);
+
+        var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(request.Password));
+
+        for (int i = 0; i < computedHash.Length; i++)
+        {
+            if (computedHash[i] != user.PasswordHash[i])
+            {
+                return Unauthorized("Invalid email or password");
+            }
+        }
+
+        return Ok(user);
+    }
+
     private async Task<bool> EmailExists(string email)
     {
         return await dbContext.Users.AnyAsync(user => user.Email.ToLower() == email.ToLower());
